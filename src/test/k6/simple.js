5799cc5870b704a179284b1a8382c98cc3a9d540
pdes1724 <116622887+pdes1724@users.noreply.github.com>
2023-05-10 14:50:35 +0530

    First Commit

---
+++ src/test/k6/simple.js
@@ -0,0 +1,20 @@
+import http from 'k6/http';
+
+export const options = {
+  vus: 10,
+  duration: '10s',
+  thresholds: {
+    'http_reqs{expected_response:true}': ['rate>10'],
+  },
+};
+
+export default function () {
+  http.get('https://test.k6.io/');
+}
+
+export function handleSummary(data) {
+  return {
+    'k6summary.html': htmlReport(data, { debug: false }),
+    stdout: textSummary(data, { indent: ' ', enableColors: true }),
+  }
+}
