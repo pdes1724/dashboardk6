9e3e7fc9570a3d30da55009c51aef9ad4e254b21
pdes1724 <Praveen.Desilva@syscolabs.com>
2023-05-11 10:50:52 +0530

    Code cahnges

--- src/test/k6/simple.js
+++ src/test/k6/simple.js
@@ -1,4 +1,6 @@
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/2.4.0/dist/bundle.js'
 import http from 'k6/http';
+import { Trend } from 'k6/metrics';
 
 export const options = {
   vus: 10,
@@ -7,6 +9,23 @@ export const options = {
     'http_reqs{expected_response:true}': ['rate>10'],
   },
 };
+
+summaryTrendStats: ["min", "max", "avg","med", "p(90)", "p(95)", "count"],
+
+let requestTrend1 = new Trend('Request1')
+
+export function contacts() {
+  let resp;
+  resp=http.get('https://test.k6.io/contacts.php')
+  requestTrend1.add(resp.timings.duration)
+}
+
+export function handleSummary(data) {
+  return {
+    'k6summary.html': htmlReport(data, { debug: false }),
+    stdout: textSummary(data, { indent: ' ', enableColors: true }),
+  }
+}
 
 export default function () {
   http.get('https://test.k6.io/');
