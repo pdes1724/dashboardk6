0910302d30a67217ffdc346e0dbf161b38b34417
pdes1724 <Praveen.Desilva@syscolabs.com>
2023-05-12 13:12:29 +0530

    perf update

--- src/test/k6/simple.js
+++ src/test/k6/simple.js
@@ -1,4 +1,5 @@
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/2.4.0/dist/bundle.js'
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js'
 import http from 'k6/http';
 import { Trend } from 'k6/metrics';
 
