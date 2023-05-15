3b30c1292a16b8b8a215e4b65f86c30bde7fae8f
pdes1724 <Praveen.Desilva@syscolabs.com>
2023-05-15 17:42:49 +0530

    script Updated

--- src/test/k6/simple.js
+++ src/test/k6/simple.js
@@ -14,7 +14,7 @@ export const options = {
   },
   tags: {
         test_name: 'TQSV-TestDB',
-        test_id: ${__ENV.TEST_ID}
+        test_id: {TEST_ID}
         //console.log('${__ENV.TEST_ID}')
     },
   summaryTrendStats: ["min", "max", "avg","med", "p(90)", "p(95)", "count"],
