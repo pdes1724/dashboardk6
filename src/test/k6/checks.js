679209282867451b58fe7780e1eab27fedddbb67
pdes1724 <Praveen.Desilva@syscolabs.com>
2023-05-18 15:24:32 +0530

    script Updated

--- src/test/k6/checks.js
+++ src/test/k6/checks.js
@@ -1,5 +1,8 @@
 import { check } from 'k6';
 import http from 'k6/http';
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/2.4.0/dist/bundle.js'
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js'
+import { Trend } from 'k6/metrics';
 
 export default function () {
   const res = http.get('http://test.k6.io/');
@@ -7,4 +10,10 @@ export default function () {
     'is status 200': (r) => r.status === 200,
     'body size is 11,105 bytes': (r) => r.body.length == 11105,
   });
+}
+export function handleSummary(data) {
+  return {
+    'k6summary.html': htmlReport(data, { debug: false }),
+    stdout: textSummary(data, { indent: ' ', enableColors: true }),
+  }
 }
