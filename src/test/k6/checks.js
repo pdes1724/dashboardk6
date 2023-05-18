78d8348169c30631308ace6750ef3d3451e446ab
pdes1724 <Praveen.Desilva@syscolabs.com>
2023-05-18 12:29:28 +0530

    script Updated

--- src/test/k6/checks.js
+++ src/test/k6/checks.js
@@ -2,7 +2,7 @@ import { check } from 'k6';
 import http from 'k6/http';
 
 export default function () {
-  const res = http.get('http://test.k6.io/');
+  const res = http.get('http://test.k.io/');
   check(res, {
     'verify homepage text': (r) =>
       r.body.includes('Collection of simple web-pages suitable for load testing'),
