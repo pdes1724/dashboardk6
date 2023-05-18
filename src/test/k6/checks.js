b9282232da2ff3312fa0fa952636af507fd32d07
pdes1724 <Praveen.Desilva@syscolabs.com>
2023-05-18 15:22:07 +0530

    script Updated

--- src/test/k6/checks.js
+++ src/test/k6/checks.js
@@ -2,9 +2,9 @@ import { check } from 'k6';
 import http from 'k6/http';
 
 export default function () {
-  const res = http.get('http://test.k.io/');
+  const res = http.get('http://test.k6.io/');
   check(res, {
-    'verify homepage text': (r) =>
-      r.body.includes('Collection of simple web-pages suitable for load testing'),
+    'is status 200': (r) => r.status === 200,
+    'body size is 11,105 bytes': (r) => r.body.length == 11105,
   });
 }
