33aab8535f39cf6b734eaa4226faa5fc7ac96f1a
pdes1724 <Praveen.Desilva@syscolabs.com>
2023-05-21 21:03:04 +0530

    script Updated

--- src/test/k6/checks.js
+++ src/test/k6/checks.js
@@ -6,9 +6,12 @@ import { Trend } from 'k6/metrics';
 
 export default function () {
   const res = http.get('http://test.k6.io/');
-
+   check(res, {
+      'is status 200': (r) => r.status === 200,
+      'body size is 11,105 bytes': (r) => r.body.length == 11105,
+      'is status 400': (r) => r.status === 400,
+    });
 }
-
 
 export function handleSummary(data) {
   return {
