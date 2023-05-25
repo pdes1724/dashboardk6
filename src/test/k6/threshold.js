54940ae73db761d2135feec8a4764d3f35ed0e14
pdes1724 <Praveen.Desilva@syscolabs.com>
2023-05-25 12:44:13 +0530

    script Updated

--- src/test/k6/threshold.js
+++ src/test/k6/threshold.js
@@ -1,5 +1,5 @@
-import http from 'k6/http';
 import { check } from 'k6';
+import http from 'k6/http';
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/2.4.0/dist/bundle.js'
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js'
 import { Trend } from 'k6/metrics';
