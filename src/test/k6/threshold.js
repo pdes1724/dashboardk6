b2f6f66ad67dff5f66f676332cf2870e404651ed
pdes1724 <Praveen.Desilva@syscolabs.com>
2023-05-25 10:37:59 +0530

    script Updated

---
+++ src/test/k6/threshold.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import http from 'k6/http';
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/2.4.0/dist/bundle.js'
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js'
+import { Trend } from 'k6/metrics';
+
+export const options = {
+  thresholds: {
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+  },
+};
+
+export default function () {
+  http.get('https://test-api.k6.io/public/crocodiles/1/');
+}
+
+
+export function handleSummary(data) {
+  return {
+    'k6summary.html': htmlReport(data, { debug: false }),
+    stdout: textSummary(data, { indent: ' ', enableColors: true }),
+  }
+}
