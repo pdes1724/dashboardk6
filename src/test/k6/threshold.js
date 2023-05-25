f00c9f4f831b506d696281515ac2ec42668681b7
pdes1724 <Praveen.Desilva@syscolabs.com>
2023-05-25 13:09:02 +0530

    script Updated

--- src/test/k6/threshold.js
+++ src/test/k6/threshold.js
@@ -1,8 +1,4 @@
-import { check } from 'k6';
 import http from 'k6/http';
-import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/2.4.0/dist/bundle.js'
-import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js'
-import { Trend } from 'k6/metrics';
 
 export const options = {
   thresholds: {
@@ -13,12 +9,4 @@ export const options = {
 
 export default function () {
   http.get('https://test-api.k6.io/public/crocodiles/1/');
-}
-
-
-export function handleSummary(data) {
-  return {
-    'k6summary.html': htmlReport(data, { debug: false }),
-    stdout: textSummary(data, { indent: ' ', enableColors: true }),
-  }
 }
