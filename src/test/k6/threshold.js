3b3d96379c62899c0930bae5e042325b70a0ade7
pdes1724 <Praveen.Desilva@syscolabs.com>
2023-05-25 17:56:22 +0530

    script Updated

--- src/test/k6/threshold.js
+++ src/test/k6/threshold.js
@@ -1,37 +1,12 @@
 import http from 'k6/http';
-import { Trend, Rate, Counter, Gauge } from 'k6/metrics';
-import { sleep } from 'k6';
 
-export const TrendRTT = new Trend('RTT');
-export const RateContentOK = new Rate('Content OK');
-export const GaugeContentSize = new Gauge('ContentSize');
-export const CounterErrors = new Counter('Errors');
 export const options = {
   thresholds: {
-    // Count: Incorrect content cannot be returned more than 99 times.
-    'Errors': ['count<100'],
-    // Gauge: returned content must be smaller than 4000 bytes
-    'ContentSize': ['value<4000'],
-    // Rate: content must be OK more than 95 times
-    'Content OK': ['rate>0.95'],
-    // Trend: Percentiles, averages, medians, and minimums
-    // must be within specified milliseconds.
-    'RTT': ['p(99)<300', 'p(70)<250', 'avg<200', 'med<150', 'min<100'],
-    // http errors should be less than 1%
-    http_req_failed: ['rate<0.01'],
-    // 95% of requests should be below 200ms
-    http_req_duration: ['p(95)<200'],
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
   },
 };
 
 export default function () {
-  const res = http.get('https://test-api.k6.io/public/crocodiles/1/');
-  const contentOK = res.json('name') === 'Bert';
-
-  TrendRTT.add(res.timings.duration);
-  RateContentOK.add(contentOK);
-  GaugeContentSize.add(res.body.length);
-  CounterErrors.add(!contentOK);
-
-  sleep(1);
+  http.get('https://test-api.k6.io/public/crocodiles/1/');
 }
