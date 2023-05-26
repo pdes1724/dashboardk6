7d65da9c8c61ec16f6d0d21a3d94ae8696fd26d5
pdes1724 <Praveen.Desilva@syscolabs.com>
2023-05-26 18:01:22 +0530

    script Updated

--- src/test/k6/threshold.js
+++ src/test/k6/threshold.js
@@ -2,12 +2,12 @@ import http from 'k6/http';
 
 export const options = {
   thresholds: {
-    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-    //http_req_failed: ['rate>0.01'], // http errors should be less than 1%
-    //http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+    http_req_failed: ['rate>0.01'], // http errors should be less than 1%
+    //http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(95)>200'], // 95% of requests should be below 200ms
   },
 };
 
 export default function () {
-  http.get('https://test-api.k.io/pudblic/crocsodiles/');
+  http.get('https://test-api.k6.io/public/crocodiles/1/');
 }
