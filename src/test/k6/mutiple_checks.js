27d270cc0d544f29a10e8c2e948fb71b202b4b3c
pdes1724 <Praveen.Desilva@syscolabs.com>
2023-05-30 15:33:19 +0530

    script Updated

--- src/test/k6/mutiple_checks.js
+++ src/test/k6/mutiple_checks.js
@@ -42,15 +42,6 @@ const res = http.get('http://test.k6.io/');
      //requestTrend1.add(res.timings.duration)
 }
 
-export default function () {
-  const res = http.get('http://test.k6.io/');
-   check(res, {
-      'is status 200': (r) => r.status === 200,
-      'body size is 11,105 bytes': (r) => r.body.length == 11105,
-      'is status 400': (r) => r.status === 400,
-    });
-}
-
 export function news() {
   const res = http.get('http://test.k6.io/');
 
