bc14096fe140477e75d22279b3f9c4e02aada738
pdes1724 <Praveen.Desilva@syscolabs.com>
2023-06-12 17:47:49 +0530

    script Updated

--- src/test/k6/mutiple_checks.js
+++ src/test/k6/mutiple_checks.js
@@ -43,7 +43,7 @@ const res = http.get('http://test.k6.io/');
 }
 
 export function news() {
-  const res = http.get('http://test.k6.io/');
+  const res = http.get('http://tes3t.k6.io/');
 
      check(res, {
         'is status 200': (r) => r.status === 200,
