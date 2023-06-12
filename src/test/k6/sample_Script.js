63ba17317378efa5a3722ed8b53d452e1078ca17
pdes1724 <Praveen.Desilva@syscolabs.com>
2023-06-12 11:40:10 +0530

    script Updated

--- src/test/k6/sample_Script.js
+++ src/test/k6/sample_Script.js
@@ -40,7 +40,7 @@ export function contacts() {
 
 export function news() {
   let resp;
-  resp=http.get('https://httpstat.us/400', {
+  resp=http.get('https://test.k6.io/newsss.php', {
     tags: { custom_tag: 'news' } ,
   });
   requestTrend2.add(resp.timings.duration)
