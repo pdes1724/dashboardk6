f0d7a51adad68a52773129b544766da8756eda48
pdes1724 <Praveen.Desilva@syscolabs.com>
2023-06-21 15:33:16 +0530

    script Updated

--- src/test/k6/sample_Script.js
+++ src/test/k6/sample_Script.js
@@ -36,10 +36,11 @@ export function contacts() {
   });
 
    check(resp, {
-        'is status 200': (r) => r.status === 200,
-        //'body size is 11,105 bytes': (r) => r.body.length == 11105,
-        'is status 400': (r) => r.status === 400,
-      });
+         'is status 200': (r) => r.status === 200,
+         'is status 400': (r) => r.status === 500,
+         //'body size is 11,105 bytes': (r) => r.body.length == 11105,
+         //'is status 400': (r) => r.status === 400,
+       });
 
   requestTrend1.add(resp.timings.duration)
 }
